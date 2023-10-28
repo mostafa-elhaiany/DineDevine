7d3461a3ba893482fbf43d9f267e638f40786afa
heidi <heidialbara>
2023-10-28 13:35:16 +0200

    initial commit

---
+++ DineDevineUI/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DineDevineUI',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
