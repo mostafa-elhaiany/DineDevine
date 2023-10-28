3200b335eb50adaf7d3c0dda29341e7162a1ce45
bethany_wong <bethanywong2163@yahoo.com>
2023-10-29 01:46:29 +0200

    cleaned up

--- FacialRecognition/extract_feature.py
+++ FacialRecognition/extract_feature.py
@@ -7,64 +7,57 @@ import cv2
 import os
 from os import listdir
 
-class ResnetFeatureExtractor:
-    def __init__(self, device='cpu'):
-        self.face_detection = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.2)
+def get_feature_vector(img, show_face_detection=False):
+    face_detection = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.2)
+    device = 'cpu'
 
-        original_model = models.resnet50(pretrained=True)
-        self.device = device
-        self.features = torch.nn.Sequential(*list(original_model.children())[:-1]).to(self.device)
-        self.features.eval()
+    original_model = models.resnet50(pretrained=False)
+    features = torch.nn.Sequential(*list(original_model.children())[:-1]).to(device)
 
-        self.preprocess = transforms.Compose([  # preprocessing for ResNet
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-    def get_feature_vector(self, image_path, show_face_detection=False):
-        '''
-        :param image_path
-        :param show_face_detection: if set to true a windows pops out with detected face
-        :return: feature vector as numpy array, None if something goes wrong
-        '''
-        img = cv2.imread(image_path)
-        if img is None:
-            print("Invalid inout path: not an image! Image: " + image_path)
-            return None
+    state_dict_path = 'resnet_features.pt'
+    if os.path.exists(state_dict_path):
+        features.load_state_dict(torch.load(state_dict_path))
+    else:
+        print("Could not find saved model weights.")
 
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    features.eval()
 
-        results = self.face_detection.process(img_rgb) # detect face
+    if img is None:
+        print("Invalid image")
+        return None
 
-        if not results.detections:
-            print("No face detected in the image.")
-            return None
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(img_rgb) # detect face
 
-        bboxC = results.detections[0].location_data.relative_bounding_box # consider first (only) face
-        ih, iw, _ = img.shape
-        x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+    if not results.detections:
+        print("No face detected in the image.")
+        return None
 
-        # -------------------- visualise detected face ---------------
-        if show_face_detection:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.imshow('Detected Face', img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-        # -----------------------------------------------------------
+    bboxC = results.detections[0].location_data.relative_bounding_box # consider first (only) face
+    ih, iw, _ = img.shape
+    x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
 
-        face_img = Image.fromarray(img_rgb[y:y + h, x:x + w])  # Crop face using bounding box
+    if show_face_detection:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow('Detected Face', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-        input_tensor = self.preprocess(face_img).unsqueeze(0).to(self.device)
+    face_img = Image.fromarray(img_rgb[y:y + h, x:x + w])
+    input_tensor = preprocess(face_img).unsqueeze(0).to(device)
 
-        with torch.no_grad():
-            features = self.features(input_tensor)
+    with torch.no_grad():
+        feature_out = features(input_tensor)
 
-        return features.squeeze().cpu().numpy()
-
-extractor = ResnetFeatureExtractor()
+    return feature_out.squeeze().cpu().numpy()
 
 #for images in os.listdir("ENFJ"):
-    #feature_vector = extractor.get_feature_vector("ENFJ/" + images, True)
-    #print(feature_vector)
+#    feature_vector = get_feature_vector(cv2.imread("ENFJ/" + images), True)
+#     print(feature_vector)
