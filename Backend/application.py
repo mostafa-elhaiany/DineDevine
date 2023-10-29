00df3c1cc683987804fd669870341e8673959426
mostafa-elhaiany <mostafa.elhayani@tum.de>
2023-10-29 15:20:01 +0100

    cors issues

--- Backend/application.py
+++ Backend/application.py
@@ -1,14 +1,17 @@
 from flask import Flask
-
+from flask_cors import CORS
 #API pages
 from API.Users import users_page
 from API.Enneagrams import enneagrams_page
 from API.Match import matches_page
 
+
 app = Flask(__name__)
 app.register_blueprint(users_page)
 app.register_blueprint(enneagrams_page)
 app.register_blueprint(matches_page)
+
+CORS(app)
 
 @app.route("/") 
 def home():
